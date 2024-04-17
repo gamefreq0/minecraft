6d779cb70540d675a9b320e8e40641806ab275d7
J. 'Dorian Greyson' L <greysondn@gmail.com>
2024-04-16 22:19:59 -0400

    Wrap earlier code in an object

--- config-src/config/ProjectE/custom_emc/resolver.py
+++ config-src/config/ProjectE/custom_emc/resolver.py
@@ -185,195 +185,194 @@ class Recipe(UpdateListener):
         
         self.isUpdating = False
 
-def graph(items:list[Item], tags:list[Tag], recipes:list[Recipe]) -> str:
-    ret:str = ""
+class Resolver():
+    def __init__(self):
+        self.items:dict[str, Item] = {}
+        self.tags:dict[str, Tag] = {}
+        self.recipes:list[Recipe] = []
+        self.recipeDict:Any = None
+        self.configDict:Any = None
     
-    # start a digraph
-    # using strict to lazily allow duplicate references
-    ret = ret + "strict digraph {\n"
-    
-    for item in items:
-        ref:str  = "i_" + str(id(item))
-        line:str = ref + "[label=\"" + item.bep + "\"]"
-        ret = ret + line + "\n"
-    
-    for tag in tags:
-        ref:str = "i_" + str(id(tag))
-        line:str = ref +  "[label=\"" + tag.bep + "\"]"
-        ret = ret + line + "\n"
-    
-    for recipe in recipes:
-        for ingredient in recipe.inputs:
-            istr:str = "i_" + str(id(ingredient.source))
-            pstr:str = "i_" + str(id(recipe.output.source))
-            line:str = istr + " -> " + pstr
+    def graph(self) -> str:
+        ret:str = ""
+        
+        # start a digraph
+        # using strict to lazily allow duplicate references
+        ret = ret + "strict digraph {\n"
+        
+        for item in list(self.items.values()):
+            ref:str  = "i_" + str(id(item))
+            line:str = ref + "[label=\"" + item.bep + "\"]"
             ret = ret + line + "\n"
+        
+        for tag in list(self.tags.values()):
+            ref:str = "i_" + str(id(tag))
+            line:str = ref +  "[label=\"" + tag.bep + "\"]"
+            ret = ret + line + "\n"
+        
+        for recipe in self.recipes:
+            for ingredient in recipe.inputs:
+                istr:str = "i_" + str(id(ingredient.source))
+                pstr:str = "i_" + str(id(recipe.output.source))
+                line:str = istr + " -> " + pstr
+                ret = ret + line + "\n"
+        
+        # end digraph
+        ret = ret + "}"
+        
+        return ret
     
-    # end digraph
-    ret = ret + "}"
+    def loadConfig(self, fpath):
+        with open(fpath) as configSrc:
+            yaml=YAML()
+            self.configDict = yaml.load(configSrc)
     
-    return ret
+    def loadRecipes(self, fpath):
+        with open(fpath) as recipeSrc:
+            yaml=YAML()
+            self.recipeDict = yaml.load(recipeSrc)
 
+    def parseRecipeDict(self):
+        for rawRecipe in tqdm.tqdm(self.recipeDict["recipes"]):
+            # we need empty recipe
+            recipe:Recipe = Recipe()
+            
+            # make sure we actually have a recipe, what the-!?!?
+            if ((rawRecipe["ingredients"] != None) and (rawRecipe["output"]["name"] != "minecraft:air")):
+                # okay. We can handle the inputs first.
+
+                for rawIngredient in rawRecipe["ingredients"]:
+                    # we have to build the structure for it - or at least make sure
+                    # it exists - and then wire it up with an ingredient into the
+                    # recipe. Also if it's already in the recipe we increase the
+                    # counter.
+                    if (rawIngredient["bep"] == "<item:minecraft:air>"):
+                        # air is used as a filler in shaped recipes
+                        # it can be safely ignored for our needs
+                        pass
+                    elif ("|" in rawIngredient["bep"]):
+                        # options
+                        optionsSwp:IngredientChoice = IngredientChoice()
+                        optionsSwp.source.bep = rawIngredient["bep"]
+                        
+                        # check to see if this one exists in recipe
+                        if recipe.hasIngredient(optionsSwp.source.bep):
+                            # get and add one to quantity
+                            swp:Ingredient = recipe.getIngredient(optionsSwp.source.bep)
+                            swp.count = swp.count + 1
+                        else:
+                            rawIngredientList = rawIngredient["bep"].split(" | ")
+                            
+                            # much of this process is echoed down below, if you need
+                            # it explained
+                            for ingredient in rawIngredientList:
+                                if (ingredient[1:4] == "tag"):
+                                    if (not ingredient in self.tags):
+                                        tagSwp:Tag = Tag()
+                                        tagSwp.bep = ingredient
+                                        self.tags[tagSwp.bep] = tagSwp
+                                    # we don't add the items to the tag here
+                                    # because we have no clue if they're
+                                    # even for a single tag or not
+                                    optionsSwp.addIngredient(self.tags[ingredient])
+                                else:
+                                    if (not ingredient in self.items):
+                                        itemSwp:Item = Item()
+                                        itemSwp.bep = ingredient
+                                        self.items[itemSwp.bep] = itemSwp
+                                    optionsSwp.addIngredient(self.items[ingredient])
+
+                    elif (rawIngredient["bep"][1:4] == "tag"):
+                        # tag
+                        
+                        # make sure tag exists first
+                        if (not rawIngredient["bep"] in self.tags):
+                            # just the tag
+                            tagSwp:Tag = Tag()
+                            tagSwp.bep = rawIngredient["bep"]
+                            self.tags[tagSwp.bep] = tagSwp
+                        
+                        # it exists, so snag it
+                        tagSwp:Tag = self.tags[rawIngredient["bep"]]
+                        
+                        # Now the items that go into it
+                        for item in rawIngredient["items"]:
+                            # rebuild the BEP string
+                            itemBep:str = "<item:" + item + ">"
+                            
+                            # create it if it doesn't exist
+                            if (not itemBep in self.items):
+                                itemSwp:Item = Item()
+                                itemSwp.bep = itemBep
+                                self.items[itemSwp.bep] = itemSwp
+                                
+                            # definitely exists, so add it to the tag
+                            tagSwp.addItem(self.items[itemBep])
+                                
+                        # tag hnow definitely exists, so... check for pre-existing
+                        # ingredient in recipe
+                        if (recipe.hasIngredient(rawIngredient["bep"])):
+                            # exists, retrieve it and increment its value
+                            ingredientSwp:Ingredient = recipe.getIngredient(rawIngredient["bep"])
+                            ingredientSwp.count = ingredientSwp.count + 1
+                        else:
+                            # doesn't exist, so create it with a count of 1 and add it
+                            # to the recipe
+                            ingredientSwp:Ingredient = Ingredient()
+                            ingredientSwp.source = self.tags[rawIngredient["bep"]]
+                            ingredientSwp.count = 1
+                            recipe.inputs.append(ingredientSwp)
+                    else:
+                        # item
+                        
+                        # make sure item exists
+                        if (rawIngredient["bep"] not in self.items):
+                            itemSwp:Item = Item()
+                            itemSwp.bep = rawIngredient["bep"]
+                            self.items[itemSwp.bep] = itemSwp
+
+                        # exists, now check for preexisting ingredient in recipe
+                        if recipe.hasIngredient(rawIngredient["bep"]):
+                            # exists, retrieve it and increment its value
+                            ingredientSwp:Ingredient = recipe.getIngredient(rawIngredient["bep"])
+                            ingredientSwp.count = ingredientSwp.count + 1
+                        else:
+                            # doesn't exist, so create it with a count of 1 and add it
+                            # to the recipe
+                            ingredientSwp:Ingredient = Ingredient()
+                            ingredientSwp.source = self.items[rawIngredient["bep"]]
+                            ingredientSwp.count = 1
+                            recipe.inputs.append(ingredientSwp)
+                
+                # that should do it. Now add the output
+                outputBep:str = "<item:" + rawRecipe["output"]["name"] + ">"
+                
+                if (outputBep not in self.items):
+                    itemSwp:Item = Item()
+                    itemSwp.bep = outputBep
+                    self.items[itemSwp.bep] = itemSwp
+                    
+                recipe.output.source = self.items[outputBep]
+                recipe.output.count = rawRecipe["output"]["count"]
+                
+                # append recipe to list
+                self.recipes.append(recipe)
+                
+    def writeGraph(self, fpath):
+        with open(fpath, "w") as graphFile:
+            graphFile.write(self.graph())
 
 def main():
-    # reference points
-    items:dict[str, Item] = {}
-    tags:dict[str, Tag] = {}
-    recipes:list[Recipe] = []
+    resolver:Resolver = Resolver()
     
-    # probaby better if we do this the sanest way possible
-    # ... with obnoxious status output
     print("Loading recipe YAML")
-    
-    recipeDict:Any = None
-    
-    with open("recipes.yaml") as recipeSrc:
-        yaml=YAML()
-        recipeDict = yaml.load(recipeSrc)
-        
+    resolver.loadRecipes("recipes.YAML")
     print("Recipe YAML loaded")
     
-    # TODO: Load config yaml, woops!
+    # TODO: load config yaml, woops!
     
     print("Parsing recipe structure")
-    
-    print("")
-    
-    for rawRecipe in tqdm.tqdm(recipeDict["recipes"]):
-        # we need empty recipe
-        recipe:Recipe = Recipe()
-        
-        # make sure we actually have a recipe, what the-!?!?
-        if ((rawRecipe["ingredients"] != None) and (rawRecipe["output"]["name"] != "minecraft:air")):
-            # okay. We can handle the inputs first.
-
-            for rawIngredient in rawRecipe["ingredients"]:
-                # we have to build the structure for it - or at least make sure
-                # it exists - and then wire it up with an ingredient into the
-                # recipe. Also if it's already in the recipe we increase the
-                # counter.
-                if (rawIngredient["bep"] == "<item:minecraft:air>"):
-                    # air is used as a filler in shaped recipes
-                    # it can be safely ignored for our needs
-                    pass
-                elif ("|" in rawIngredient["bep"]):
-                    # options
-                    optionsSwp:IngredientChoice = IngredientChoice()
-                    optionsSwp.source.bep = rawIngredient["bep"]
-                    
-                    # check to see if this one exists in recipe
-                    if recipe.hasIngredient(optionsSwp.source.bep):
-                        # get and add one to quantity
-                        swp:Ingredient = recipe.getIngredient(optionsSwp.source.bep)
-                        swp.count = swp.count + 1
-                    else:
-                        rawIngredientList = rawIngredient["bep"].split(" | ")
-                        
-                        # much of this process is echoed down below, if you need
-                        # it explained
-                        for ingredient in rawIngredientList:
-                            if (ingredient[1:4] == "tag"):
-                                if (not ingredient in tags):
-                                    tagSwp:Tag = Tag()
-                                    tagSwp.bep = ingredient
-                                    tags[tagSwp.bep] = tagSwp
-                                # we don't add the items to the tag here
-                                # because we have no clue if they're
-                                # even for a single tag or not
-                                optionsSwp.addIngredient(tags[ingredient])
-                            else:
-                                if (not ingredient in items):
-                                    itemSwp:Item = Item()
-                                    itemSwp.bep = ingredient
-                                    items[itemSwp.bep] = itemSwp
-                                optionsSwp.addIngredient(items[ingredient])
-
-                elif (rawIngredient["bep"][1:4] == "tag"):
-                    # tag
-                    
-                    # make sure tag exists first
-                    if (not rawIngredient["bep"] in tags):
-                        # just the tag
-                        tagSwp:Tag = Tag()
-                        tagSwp.bep = rawIngredient["bep"]
-                        tags[tagSwp.bep] = tagSwp
-                    
-                    # it exists, so snag it
-                    tagSwp:Tag = tags[rawIngredient["bep"]]
-                    
-                    # Now the items that go into it
-                    for item in rawIngredient["items"]:
-                        # rebuild the BEP string
-                        itemBep:str = "<item:" + item + ">"
-                        
-                        # create it if it doesn't exist
-                        if (not itemBep in items):
-                            itemSwp:Item = Item()
-                            itemSwp.bep = itemBep
-                            items[itemSwp.bep] = itemSwp
-                            
-                        # definitely exists, so add it to the tag
-                        tagSwp.addItem(items[itemBep])
-                            
-                    # tag hnow definitely exists, so... check for pre-existing
-                    # ingredient in recipe
-                    if (recipe.hasIngredient(rawIngredient["bep"])):
-                        # exists, retrieve it and increment its value
-                        ingredientSwp:Ingredient = recipe.getIngredient(rawIngredient["bep"])
-                        ingredientSwp.count = ingredientSwp.count + 1
-                    else:
-                        # doesn't exist, so create it with a count of 1 and add it
-                        # to the recipe
-                        ingredientSwp:Ingredient = Ingredient()
-                        ingredientSwp.source = tags[rawIngredient["bep"]]
-                        ingredientSwp.count = 1
-                        recipe.inputs.append(ingredientSwp)
-                else:
-                    # item
-                    
-                    # make sure item exists
-                    if (rawIngredient["bep"] not in items):
-                        itemSwp:Item = Item()
-                        itemSwp.bep = rawIngredient["bep"]
-                        items[itemSwp.bep] = itemSwp
-
-                    # exists, now check for preexisting ingredient in recipe
-                    if recipe.hasIngredient(rawIngredient["bep"]):
-                        # exists, retrieve it and increment its value
-                        ingredientSwp:Ingredient = recipe.getIngredient(rawIngredient["bep"])
-                        ingredientSwp.count = ingredientSwp.count + 1
-                    else:
-                        # doesn't exist, so create it with a count of 1 and add it
-                        # to the recipe
-                        ingredientSwp:Ingredient = Ingredient()
-                        ingredientSwp.source = items[rawIngredient["bep"]]
-                        ingredientSwp.count = 1
-                        recipe.inputs.append(ingredientSwp)
-            
-            # that should do it. Now add the output
-            outputBep:str = "<item:" + rawRecipe["output"]["name"] + ">"
-            
-            if (outputBep not in items):
-                itemSwp:Item = Item()
-                itemSwp.bep = outputBep
-                items[itemSwp.bep] = itemSwp
-                
-            recipe.output.source = items[outputBep]
-            recipe.output.count = rawRecipe["output"]["count"]
-            
-            # append recipe to list
-            recipes.append(recipe)
-        
-    print("")
-    print("outputting graph")
-    
-    with open("graph.dot", "w") as graphFile:
-        iList:list[Item] = list(items.values())
-        tList:list[Tag]  = list(tags.values())
-        
-        graphFile.write(graph(iList, tList, recipes))
-        
-    print("Done!")
+    resolver.parseRecipeDict()
     
 if (__name__ == "__main__"):
     main()
